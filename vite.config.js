426dd5568cb25570b583a1d0e414f544295de814
Bryan Ramirez-Gonzalez <bryanram2024@gmail.com>
2024-10-04 06:44:46 -0400

    Added Estimathon

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/', // Replace <REPO_NAME> with your GitHub repository name
-});
